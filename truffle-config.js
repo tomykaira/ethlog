44ecdd7537e167a9719f03bb3b748918c229206a
tomykaira <tomykaira@gmail.com>
2019-04-25 03:52:14 +0900

    deploy

--- truffle-config.js
+++ truffle-config.js
@@ -62,7 +62,7 @@ module.exports = {
     ropsten: {
      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`),
      network_id: 3,       // Ropsten's id
-     gas: 5500000,        // Ropsten has a lower block limit than mainnet
+     gas:  4700000,        // Ropsten has a lower block limit than mainnet
      confirmations: 0,    // # of confs to wait between deployments. (default: 0)
      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
